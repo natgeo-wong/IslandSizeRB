7ad48068ec1556b689cd5387b301167a1f7e0505
Nathanael Wong <natgeo.wong@outlook.com>
2023-03-09 13:56:38 -0500

    Extending the model, make it GPU()?

--- scripts/samplerayleighbenard.jl
+++ scripts/samplerayleighbenard.jl
@@ -1,52 +1,66 @@
 using DrWatson
 @quickactivate "IslandSizeRB"
 
+using Dates
+using Logging
 using Oceananigans
+using Printf
+
+processor = CPU()
+# processor = GPU()
+
+xmin,xmax = 0,6000; nx = 6000
+ymin,ymax = -100,100; ny = 2000
+zmin,zmax = 0,1; ny = 64
 
 grid = RectilinearGrid(
-    # size=(256,256,64), x=(0,4), y=(0,4), z=(0,1),
-    size=(1024,64), x=(0,256), z=(0,1),
+    processor,
+    size = (nx,ny),    x = (xmin,xmax), y = (ymin,ymax),
     topology=(Periodic, Flat, Bounded)
+    # size = (nx,ny,nz), x = (xmin,xmax), y = (ymin,ymax), z = (zmin,zmax),
+    # topology=(Periodic, Periodic, Bounded)
 )
 
 b_bc_top = ValueBoundaryCondition(0)
 b_bc_bot = ValueBoundaryCondition(1)
 b_bcs = FieldBoundaryConditions(top=b_bc_top, bottom=b_bc_bot)
 
-Ra = 1e7
-Pr = 1
+const Ra = 1e7
+const Pr = 1
 
 # Ra = 1 / (κ² * Pr)
 #   -> κ = sqrt(1 / (Ra * Pr))
-κ = sqrt(1 / (Ra * Pr))
-ν = κ * Pr
+const κ = sqrt(1 / (Ra * Pr))
+const ν = κ * Pr
 
-model = NonhydrostaticModel(; grid,
-                            timestepper = :RungeKutta3,
-                            tracers = :b,
-                            buoyancy = BuoyancyTracer(),
-                            boundary_conditions = (; b=b_bcs),
-                            closure = ScalarDiffusivity(; ν, κ))
+model = NonhydrostaticModel(;
+    grid,
+    timestepper = :RungeKutta3,
+    advection = WENO(),
+    tracers = :b,
+    buoyancy = BuoyancyTracer(),
+    boundary_conditions = (; b=b_bcs),
+    closure = ScalarDiffusivity(; ν, κ)
+)
 
-wᵢ(x, y, z) = 1e-3 * (rand() - 1)
-set!(model, w=wᵢ)
-
-simulation = Simulation(model, Δt=1e-2, stop_time=100)
+wᵢ(x,y,z) = 1e-3 * (rand()-1); set!(model, w=wᵢ)
 
 wizard = TimeStepWizard(cfl=0.7, max_change=1.1)
-simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
 
-progress(sim) = @info string("Iter: ", iteration(sim),
-                             ", time: ", time(sim),
-                             ", max w: ", maximum(abs, model.velocities.w))
+start_time = time_ns()
+progress(sim) = @info "$(now()) - Oceananigans.jl - Integration completed through $(@sprintf("%07d",sim.model.clock.iteration)) steps | Model Time: $(@sprintf("%09.3f",sim.model.clock.time))"
 
+simulation = Simulation(model, Δt=1e-2, stop_time=100)
+simulation.callbacks[:wizard]   = Callback(wizard,   IterationInterval(10))
 simulation.callbacks[:progress] = Callback(progress, IterationInterval(10))
 
 u, v, w = model.velocities # unpack velocity `Field`s
 b = model.tracers.b        # unpack buoyancy `Field`
 
 simulation.output_writers[:field_writer] = NetCDFOutputWriter(
-    model, (; u, v, w, b), filename=datadir("more_fields.nc"), overwrite_existing=true,
+    model, (; u, v, w, b),
+    filename=datadir("more_fields.nc"),
+    overwrite_existing=true,
     schedule=TimeInterval(0.1)
 )
 
